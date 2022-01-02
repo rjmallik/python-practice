4be3cf345a6ab02327f206490671226fc4f6f08e
RJ Mallik <rutujit97872@gmail.com>
2022-01-01 18:22:26 -0800

    new changes on for loop

--- ifstatements.py
+++ ifstatements.py
@@ -1,3 +1,4 @@
+"""
 a=30
 b=10
 if b==a:
@@ -5,4 +6,28 @@ if b==a:
 elif b>a:
     print("b is greater than a")
 else:
-    print("a is greater than bbb")
+    print("a is greater than b")
+i=0
+while i<6:
+    print(i)
+    i=i+1
+    if i==3:
+        continue
+else:
+    print("that number is too big")
+
+randomlist=["cherry", "banana","apple", 12,49,"98"]
+for x in randomlist:
+    print(x)
+"""
+g= "plebian"
+for x in g:
+    print(x)
+    if x != "b":
+        break
+#print(x)
+
+#g="banana"
+#for x in g:
+ #   print(x)
+
