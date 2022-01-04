7de0871a724dd89abb14e4caf235e7508e5a9ced
RJ Mallik <rutujit97872@gmail.com>
2022-01-03 20:28:24 -0800

    changes on how inheritance works

--- classes_objectsPractice.py
+++ classes_objectsPractice.py
@@ -19,18 +19,20 @@ print(hB1.age)
 print(hB1.haircolor)
 """
 class teenager(humanBeing):
-    def __init__(self,name,age,haircolor, height):
+    def __init__(self,name,age,height, haircolor="black" ):
         humanBeing.__init__(self,name,age,haircolor)
-        # or you can add this function: humanBeing.__init__(self,name,age,haircolor)
+        # or you can add super function: super().__init__(self,name,age,haircolor)
         self.height= height # in inches
-        
+        self.misc="Miscellaneous"
     def welcomeStatement(self):
         print ("Hello I am ", self.name, "and am", self.height, "inches tall!")
 
-footballTeen=teenager("Jack", 18, "blonde",68 )
+footballTeen=teenager("Jack", 18, 68, "blonde")
+footballTeen.welcomeStatement()
+
 #print(footballTeen.height)
 #print(footballTeen.name)
-t1=teenager("Ruthika", 11,"black", 60)
-#print(t1.haircolor)
+t1=teenager("Ruthika", 11, 60)
+print(t1.haircolor)
 
-footballTeen.welcomeStatement()
+
