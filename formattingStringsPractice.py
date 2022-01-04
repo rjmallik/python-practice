22051c33d5f2d1478d3703d7d7e2383ea2651102
RJ Mallik <rutujit97872@gmail.com>
2022-01-03 20:05:20 -0800

    PRacticing on ways to format Strings

---
+++ formattingStringsPractice.py
@@ -0,0 +1,14 @@
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
+
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
